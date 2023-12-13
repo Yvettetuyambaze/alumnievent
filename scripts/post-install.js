17f1b0983c961136c6a5f50424cd901fda2cfdb1
Yvettetuyambaze <tuyambazeyvette@gmail.com>
2023-12-13 22:58:51 +0400

    removed errors

--- scripts/post-install.js
+++ scripts/post-install.js
@@ -1,6 +1,6 @@
 // ABOUT CODES
-// the purpose of the code, which involves creating an environment configuration file 
-// from a provided template and handling any errors that might occur during the process.
+// The purpose of the code is to create an environment configuration file 
+// from a provided template and handle any errors that might occur during the process.
 
 const fs = require('fs-extra')
 
@@ -8,19 +8,5 @@ try {
     fs.copySync('./config/config.env.example', './config/config.env')
     console.log('Environment file created successfully.')
 } catch (err) {
-    console.error(err)
-}
-// Import the fs-extra module for file system operations
-const fs = require('fs-extra')
-
-// Attempt to copy the configuration file template to the actual configuration file
-try {
-    // Copy the contents of the example environment file to the actual environment file
-    fs.copySync('./config/config.env.example', './config/config.env')
-
-    // Log a success message if the operation was successful
-    console.log('Environment file created successfully.')
-} catch (err) {
-    // Log any errors that occur during the file copy operation
     console.error(err)
 }
