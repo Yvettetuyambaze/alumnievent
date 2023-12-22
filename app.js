7acf3fab892984d417c2ececcb6a28978545dbdc
Yvettetuyambaze <tuyambazeyvette@gmail.com>
2023-12-22 18:38:51 +0400

    updated app.js

--- app.js
+++ app.js
@@ -131,5 +131,5 @@ const PORT = process.env.PORT || 3000; // Define the port number
 // Start the server
 app.listen(
   PORT,
-  console.log(`The Server is running in ${process.env.NODE_ENV} mode on port ${PORT}...`) // Log server start information
+  console.log(`The Server is running on port ${PORT}...`) // Log server start information
 );
