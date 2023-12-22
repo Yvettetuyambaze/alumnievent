d0bc5ce99907e7ce052f11a688df94827eacbcf3
Yvettetuyambaze <tuyambazeyvette@gmail.com>
2023-12-22 18:33:11 +0400

    corrected some error for deployment

--- config/database.js
+++ config/database.js
@@ -12,10 +12,7 @@ const mongoose = require('mongoose')
 const connectDB = async () => {
   try {
     // Using the mongoose connect method to connect to the provided MongoDB URI
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    const conn = await mongoose.connect(process.env.MONGO_URI);
 
     // Logging a success message along with the host of the connected MongoDB database
     console.log(`MongoDB Connected Successfully: ${conn.connection.host}`)
